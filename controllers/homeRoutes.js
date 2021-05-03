6764bcd34d5518e67c9012c0708794a5460121dd
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-02 18:25:21 -0700

    add root routing and start homepage html with handlebars, write seed function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,30 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
 const { Comment, Post, User} = require('../models');
+
+router.get('/', async (req, res)=> {
+    try {
+        const postData = await Post.findAll({});
+        const posts = postData.map((post) => post.get({ plain: true }));
+        console.log(posts);
+
+        res.render('homepage', {
+            post: posts, 
+            // logged_in: req.session.logged_in 
+        });
+
+    } catch (err) {
+    res.status(500).json(err);
+    }
+
+
+
+})
 // require models
 
 
+//get / root request
+// get /login request
+
+
+//export as router
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,36 @@
 const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./projectData.json');
+const commentData = require('./commentData.json');
+
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Post.bulkCreate(postData, {
+        returning: true,
+    });
+
+    await Comment.bulkCreate(commentData, {
+        returning: true,
+    });
+
+  process.exit(0);
+};
+
+seedDatabase();
+
+//user bulkcreate individualHooks true returning true
+
+//post bulkcreate
+
 //require models 
 
 //require data.json objects for each object to seed
