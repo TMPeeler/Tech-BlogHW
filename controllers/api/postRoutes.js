10a6075b2c26cecf617f78ba6cfe547d2e54884e
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-03 15:20:44 -0700

    begin working on new post and comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -33,7 +33,17 @@ const withAuth = require('../../utils/auth');
     }
 });
 
+router.post('/new', async (req, res) => {
+  try {
 
+    const postData = await Post.create(req.body);
+    res.status(200).json(postData);
+
+  } catch (err) {
+    res.status(400).json(err);
+
+  }
+}
 // router.get('/dashboard', withAuth, async, (req, res) => {
 
 //     const postData = await Post.findAll({
